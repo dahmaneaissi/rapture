77ad0b5953089dd6af08dc833344a5553d5921bf
dahmane <aissi.dahmane@gmail.com>
2017-03-27 00:50:26 +0100

    ++

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('birthday');
             $table->integer('willaya');
-            $table->string('fb_profil');
             $table->string('password', 60);
             $table->string('role');
             $table->integer('medias_id');
