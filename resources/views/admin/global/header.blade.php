7414a2106b564bb1645f0a09230dcaf99f506c52
Dahmane Aissi <aissi@allegorie.tv>
2017-05-30 14:32:40 +0100

    +++

--- resources/views/admin/global/header.blade.php
+++ resources/views/admin/global/header.blade.php
@@ -38,9 +38,6 @@
 
                         <!-- Menu Footer-->
                         <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                            </div>
                             <div class="pull-right">
                                 <a href="{{ action('Auth\AuthController@getLogout') }}" class="btn btn-default btn-flat">Sign out</a>
                             </div>
