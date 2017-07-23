b8e8571ae0a946d7f10d6f36dbc45dd7624f84f4
dahmane <aissi.dahmane@gmail.com>
2017-07-23 23:27:37 +0100

    [dev] Change user seeders

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,13 +15,8 @@ class UserTableSeeder extends Seeder {
                 'id'        => '1',
                 'firstname' => 'Dahmane',
                 'lastname'  => 'Aissi',
-                'tel'       => '0770714662',
                 'email'     => 'aissi.dahmane@gmail.com',
-                'willaya'   => 16,
-                'birthday'  => '15-12-1987',
-                'role'      => 'administrator',
                 'password'  => bcrypt( 'khNPqKrSKdfm' ),
-                'medias_id'  => 99999999
             ]
         );
     }
